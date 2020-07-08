41f7d8ee1db5adad44a81a52444598f11cc90b2f
christian <chirssaher@gmail.com>
2020-07-08 17:44:52 +0900

    add saveChunk method on server api

--- srcapi/app.js
+++ srcapi/app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const mongoose = require ('mongoose');
 const formidable = require('formidable');
+const md5 = require('js-md5');
+const fs = require('fs')
 const router = express.Router();
 const app = express();
-var datetime = new Date();
 
 
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded());
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.set('port', process.env.PORT || 3000);
 
@@ -21,6 +20,7 @@ const fileprueba = require('./models/fileup');
 
 // add reference to filemetadata
 const filemetadata = require('./dbconn/file_metadata.js')
+const chunkdata = require('./dbconn/chunk_data.js')
 
 
 app.get('/', (req, res) => {
@@ -38,11 +38,12 @@ app.post('/do-upload',  (req, res) => {
     console.log('size:',files.upload.size);
   });
 
-
   form.on('fileBegin', function (name, file) {
     file.path = __dirname + '/uploadfiletest/' + file.name
-  })
 
+		let arrayBuffer = FileReader().readAsArrayBuffer(file)
+		console.log("arrayBuffer: ", arrayBuffer);
+  })
   res.send("Files Uploaded")
 })
 
@@ -62,6 +63,43 @@ app.post('/createFile', (req, res) => {
 	// set a response
 	res.status(resCode).send(resData)
 });
+
+app.post('/saveChunk', (req, res) => {
+	let hashId = req.body.hashId
+	let position = req.body.position
+	let chunk = req.body.chunk
+
+	let buffer = str2ab(chunk)
+	// get the checksum - already confirmed with other tools
+	let checksum = md5(Buffer.from(buffer))
+
+	// try insert into db
+	var values = chunkdata.create(checksum, chunk)
+	var resCode = values[0]
+	var resData = values[1]
+
+	if(resCode == 500) {
+		res.status(resCode).send(resData)
+		return
+	}
+
+	// try update the original file
+	values = fileMetadata.update(hashId, checksum, position)
+	resCode = values[0]
+	resData = values[1]
+
+	// set a response
+	res.status(resCode).send(resData)
+});
+
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+  var bufView = new Uint8Array(buf);
+  for (var i=0, strLen=str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
 
 //server start
 app.listen(app.get('port'),() => {
