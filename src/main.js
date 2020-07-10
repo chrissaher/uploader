0c1cb4cd7c3e6882e1ab1a4105c3f1ddfa6edc4a
Aleup <alllantoy@gmail.com>
2020-07-10 18:38:13 -0500

    FIX Format of Date of List View / Upload Date using MomentoJS

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueMoment from 'vue-moment'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueMoment);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
